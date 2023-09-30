1f29202a16431df440e3c67dcfd596c1383b8ea1
Duckwings <bmmanaois@flash.net>
2023-09-30 18:04:04 -0500

    Move player spawning into game script

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const map_selection_scene: PackedScene = preload("res://scenes/ui/map_selection/map_selection.tscn")
 var map_selection_instance = map_selection_scene.instantiate()
+const player_scene: PackedScene = preload("res://scenes/player/player.tscn")
 
 const lava_map_scene: PackedScene = preload("res://scenes/maps/lava_map/lava_map.tscn")
 
@@ -16,3 +17,6 @@ func _process(delta):
 
 func start_game(map):
 	add_child(map)
+	var player_instance = player_scene.instantiate()
+	map.add_child(player_instance)
+	player_instance.global_position = map.get_node("PlayerSpawnpoint").global_position
