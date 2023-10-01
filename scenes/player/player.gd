0611577b7b9f78f0c0e0c164c4371f33f29e53fb
Duckwings <bmmanaois@flash.net>
2023-10-01 18:17:49 -0500

    Fancy player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,7 +3,14 @@ extends CharacterBody2D
 @onready var model_anim = get_node("SubViewport/PlayerModel/lava_boy_skeleton/AnimationPlayer")
 const SPEED = 300.0
 const JUMP_VELOCITY = -575.0
+var coyote_time = 100
 
+var direction: float = 1
+var acceleration: float = 600
+var deceleration: float = 3
+var speed_cap: float = 300
+
+var last_on_floor: int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,29 +22,16 @@ func _process(_delta):
 	$SprPlayer.scale = Vector2( 1152.0 / ws.x, 648.0 / ws.y)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		model_anim.play("jump")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		if velocity.x < 0:
-			$SprPlayer.flip_h = true
-		else:
-			$SprPlayer.flip_h = false
-		velocity.x = direction * SPEED
+	move_player(delta)
+	
+	
+	if direction == -1:
+		$SprPlayer.flip_h = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$SprPlayer.flip_h = false
 		
 	if is_on_floor() and not model_anim.is_playing() and not model_anim.current_animation == "jump":
-		if direction:
+		if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
 			model_anim.play("renderwire")
 		else:
 			model_anim.play("renderwire")
@@ -50,10 +44,59 @@ func _physics_process(delta):
 		model_anim.stop()
 	elif not is_on_floor() and velocity.y > 0 and not model_anim.current_animation == "jump" and not model_anim.current_animation == "Skeleton_001Action":
 		model_anim.play("Skeleton_001Action")
-		
+	
+	
+	move_and_slide()
+
+
+func move_player(delta):
+	# Drop through platforms
 	if Input.is_action_pressed("down"):
 		set_collision_mask_value(1, false)
 	else:
 		set_collision_mask_value(1, true)
 	
-	move_and_slide()
+	# Set direction
+	if Input.is_action_just_pressed("left") or (Input.is_action_pressed("left") and not Input.is_action_pressed("right")):
+		direction = -1
+	if Input.is_action_just_pressed("right") or (Input.is_action_pressed("right") and not Input.is_action_pressed("left")):
+		direction = 1
+	
+	# Move left/right
+	if Input.is_action_pressed("left") and direction == -1:
+		velocity.x -= acceleration * delta
+	if Input.is_action_pressed("right") and direction == 1:
+		velocity.x += acceleration * delta
+	
+	# Cap horizontal speed
+	if velocity.x > speed_cap:
+		velocity.x = speed_cap
+	if velocity.x < -speed_cap:
+		velocity.x = -speed_cap
+	
+	# Decelerate toward 0 when not holding left
+	if not Input.is_action_pressed("left") or direction == 1:
+		if velocity.x > -acceleration * deceleration * delta and velocity.x < 0:
+			velocity.x = 0
+		if velocity.x <= -acceleration * deceleration * delta:
+			velocity.x += acceleration * deceleration * delta
+	
+	# Decelerate toward 0 when not holding right
+	if not Input.is_action_pressed("right") or direction == -1:
+		if velocity.x < acceleration * deceleration * delta and velocity.x > 0:
+			velocity.x = 0
+		if velocity.x >= acceleration * deceleration * delta:
+			velocity.x -= acceleration * deceleration * delta
+	
+	# Coyote time
+	if is_on_floor():
+		last_on_floor = Time.get_ticks_msec()
+	
+	# Jump
+	if Input.is_action_just_pressed("jump") and Time.get_ticks_msec() - last_on_floor <= coyote_time:
+		velocity.y = JUMP_VELOCITY
+		model_anim.play("jump")
+	
+	# Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
