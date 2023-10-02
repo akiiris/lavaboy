a765981db2acb9ca7d37128dfeb64981aab74eba
Jivvy <dd300268@gmail.com>
2023-10-02 04:43:32 -0500

    Add death sound

--- assets/menu/death_screen/death_screen.gd
+++ assets/menu/death_screen/death_screen.gd
@@ -4,3 +4,7 @@ func _on_again_button_button_up():
 	if $AgainButton.is_hovered():
 		get_tree().root.get_node("Game").start_game()
 		queue_free()
+
+
+func _on_music_start_timer_timeout():
+	$Music.play()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const death_screen = preload("res://assets/menu/death_screen/death_screen.tscn")
+const death_sound_scene = preload("res://scenes/player/player_death_sound.tscn")
 
 @onready var model_anim = get_node("SubViewport/lava_boy_skeleton/AnimationPlayer")
 const SPEED = 300.0
@@ -116,6 +117,10 @@ func die():
 	var dsi = death_screen.instantiate()
 	dsi.get_node("Time").text = str(time) + " s"
 	get_tree().root.add_child(dsi)
+	var dsoundi = death_sound_scene.instantiate()
+	get_tree().root.add_child(dsoundi)
+	
+	
 
 func collect_wisp():
 	wisps += 1
