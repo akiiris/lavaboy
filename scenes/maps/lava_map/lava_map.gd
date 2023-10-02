a0f51a724ae83a91d3db70b463116ad8493fea07
Duckwings <bmmanaois@flash.net>
2023-10-01 23:24:51 -0500

    Add wisp spawning

--- scenes/maps/lava_map/lava_map.gd
+++ scenes/maps/lava_map/lava_map.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+var wisp_scene: PackedScene = preload("res://scenes/wisp/wisp.tscn")
+
 var time_started
 var time_score_thing
+
+const WISP_SPAWN_CAP: int = 10
+
 
 func _ready():
 	time_started = Time.get_ticks_msec()
@@ -10,3 +15,15 @@ func _ready():
 func _process(delta):
 	time_score_thing = (Time.get_ticks_msec()-time_started)/1000.0
 	get_node("Player/Camera/Score").text = str(time_score_thing) + "s"
+	print($Wisps.get_children().size())
+
+
+func spawn_wisp():
+	var wisp_instance = wisp_scene.instantiate()
+	$Wisps.add_child(wisp_instance)
+	wisp_instance.global_position = Vector2(randf_range($WispSpawning/Marker1.global_position.x, $WispSpawning/Marker2.global_position.x), randf_range($WispSpawning/Marker1.global_position.y, $WispSpawning/Marker2.global_position.y))
+
+
+func _on_wisp_spawn_timer_timeout():
+	if $Wisps.get_children().size() < WISP_SPAWN_CAP:
+		spawn_wisp()
