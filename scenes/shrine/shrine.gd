4694cc18047410e8d73813418aeabe9087a41485
Jivvy <dd300268@gmail.com>
2023-10-02 05:55:59 -0500

    Add more sounds, shrine effects

--- scenes/shrine/shrine.gd
+++ scenes/shrine/shrine.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
+
 func _process(_delta):
 	$Sprite2D.texture = $SubViewport.get_texture()
 	var ws = DisplayServer.window_get_size()
 	$SubViewport.size = ws
 	$Sprite2D.scale = Vector2( 1152.0 / ws.x, 648.0 / ws.y)
+
+
+func interact(user):
+	if user.has_method("try_consume_wisp"):
+		if user.try_consume_wisp(1):
+			activate()
+		else:
+			$FailSound.play()
+
+
+func activate():
+	$ActivateSound.play()
+	$SubViewport/ActivateParticles.restart()
+	$SubViewport/ActivateParticles.emitting = true
+
+
+func _on_interact_area_area_entered(area):
+	$SubViewport/DirectionalLight3D.light_energy = 3
+
+
+func _on_interact_area_area_exited(area):
+	$SubViewport/DirectionalLight3D.light_energy = 1
