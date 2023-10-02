a2f944477a34f6bafcaa54840e0ffef8dd6f939c
Jivvy <dd300268@gmail.com>
2023-10-02 02:37:08 -0500

    Redo main menu a bit

--- scenes/ui/map_selection/map_selection.gd
+++ scenes/ui/map_selection/map_selection.gd
@@ -4,6 +4,15 @@ extends Node2D
 
 const lava_map_scene: PackedScene = preload("res://scenes/maps/lava_map/lava_map.tscn")
 
+func _ready():
+	$SubViewport/lava_boy_skeleton4.get_node("AnimationPlayer").play("renderwire_001")
+
+func _process(delta):
+	$Kunti.texture = $SubViewport.get_texture()
+	var ws = DisplayServer.window_get_size()
+	$SubViewport.size = ws
+	$Kunti.scale = Vector2(1152.0 / ws.x, 648.0 / ws.y)
+
 
 func _on_btn_play_pressed():
 	if $BtnPlay.is_hovered():
